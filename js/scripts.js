fb0242f2058ed79e6e9e8d69e0979d24edca7fcf
twoodman <tdwoodman@outlook.com>
2016-09-07 02:50:56 -0600

    add mobile nav functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,35 @@
+// put mobile nav in a var
+var $mobNav = $('#mobile-nav');
+// mobile nav toggle
+$('#mobile-nav__button').on('click', function(e) {
+  e.preventDefault();
+  // if its open
+  if ($mobNav.hasClass('active')) {
+    // close it
+    $mobNav.removeClass('active');
+  } else {
+    // else open it
+    $mobNav.addClass('active');
+  }
+});
+
+// close mobile nav on link click
+$('#mobile-nav li a').on('click', function(e) {
+  e.preventDefault();
+  // close it
+  $mobNav.removeClass('active');
+});
+
+// anchor scroll
 $('a[href^="#"]').on('click',function (e) {
     e.preventDefault();
-
+    // get href starting at hash
     var target = this.hash;
+    // wrap it in a jquery object
     var $target = $(target);
-
+    // animate scroll
     $('html, body').stop().animate({
         'scrollTop': $target.offset().top
-    }, 800, 'swing');
+    // make it super fast
+    }, 400, 'swing');
 });
