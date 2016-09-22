20b754d4eb9f942ab390b809c62c8c650f653fa5
twoodman <tdwoodman@outlook.com>
2016-09-22 11:07:40 -0600

    rewrite jquery script to pure JS/ES6 remove scroll jack delete minified js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,29 @@
-// put mobile nav in a var
-var $mobNav = $('#mobile-nav');
-// mobile nav toggle
-$('#mobile-nav__button').on('click', function(e) {
-  e.preventDefault();
-  // if its open
-  if ($mobNav.hasClass('active')) {
-    // close it
-    $mobNav.removeClass('active');
-  } else {
-    // else open it
-    $mobNav.addClass('active');
+(() => {
+  // grab mobile nav
+  const mobNav = document.querySelector('.mobile-nav')
+  // grab mobile nav button
+  const mobNavButton = document.querySelector('.mobile-nav__button')
+  // grab mobile nav links
+  const mobNavLinks = document.querySelectorAll('.mobile-nav__list-link')
+
+  // on mobile nav button click
+  mobNavButton.addEventListener('click', (event) => {
+    event.preventDefault()
+    // if its open
+    if (mobNav.classList.contains('active')) {
+      // close it
+      mobNav.classList.remove('active')
+    } else {
+      // otherwise open it
+      mobNav.classList.add('active')
+    }
+  })
+
+  // add click event listeners to each mobile nav link
+  for (let i = 0; i < mobNavLinks.length; i++) {
+    mobNavLinks[i].addEventListener('click', () => {
+      // close mobile nav on link click
+      mobNav.classList.remove('active')
+    })
   }
-});
-
-// close mobile nav on link click
-$('#mobile-nav ul li a').on('click', function(e) {
-  e.preventDefault();
-  // close it
-  $mobNav.removeClass('active');
-});
-
-// anchor scroll
-$('a[href^="#"]').on('click',function (e) {
-    e.preventDefault();
-    // get href starting at hash
-    var target = this.hash;
-    // wrap it in a jquery object
-    var $target = $(target);
-    // animate scroll
-    $('html, body').stop().animate({
-        'scrollTop': $target.offset().top
-    // make it super fast
-    }, 400, 'swing');
-});
+})()
